3359184b9862aa625951d2ca4d506ec2a43105a6
林枫 <33085678+13250203323@users.noreply.github.com>
2017-10-27 17:04:44 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -10,11 +10,7 @@ skynet.start(function()
 		local console = skynet.newservice("console")
 	end
 	skynet.newservice("debug_console",8000)
-	
 	skynet.uniqueservice("protoloader")
-	-- skynet.uniqueservice("roommanager")
-	-- skynet.newservice("roledtdb")
-	-- skynet.newservice("simpledb")
 
 	local watchdog = skynet.newservice("watchdog")
 	skynet.call(watchdog, "lua", "start", {
