542578573e205e9c0b6c73b5af0bc6260dca2647
林枫 <33085678+13250203323@users.noreply.github.com>
2017-11-07 19:10:10 +0800

    Add files via upload

--- logic/define.lua
+++ logic/define.lua
@@ -13,13 +13,14 @@ CARD_JOKER = 5 				-- 王
 -- 出牌基本类型
 HANDOUT_DANGE = 100  		-- 单个
 HANDOUT_DUIZI = 101  		-- 对子
-HANDOUT_SHUNZI = 102  		-- 顺子
-HANDOUT_LIANDUI = 103  		-- 连对
-HANDOUT_SANDAI_1 = 104  	-- 三带单
-HANDOUT_SANDAI_2 = 105  	-- 三带对子
-HANDOUT_FEIJI_1 = 106 		-- 飞机带单
-HANDOUT_FEIJI_2 = 107  		-- 飞机带对子
+HANDOUT_SANGE = 102  		-- 三个
+HANDOUT_SHUNZI = 103  		-- 顺子
+HANDOUT_LIANDUI = 104  		-- 连对
+HANDOUT_SANDAI_1 = 105  	-- 三带单
+HANDOUT_SANDAI_2 = 106  	-- 三带对子
+HANDOUT_FEIJI_1 = 107 		-- 飞机带单
+HANDOUT_FEIJI_2 = 108  		-- 飞机带对子
 HANDOUT_SIDAIER = 108  		-- 四带二
-HANDOUT_ZHADAN = 109 		-- 炸弹
-HANDOUT_HUOJIAN = 110  		-- 火箭
+HANDOUT_ZHADAN = 110 		-- 炸弹
+HANDOUT_HUOJIAN = 111  		-- 火箭
 
--- logic/handoutcard.lua
+++ logic/handoutcard.lua
@@ -75,7 +75,7 @@ local function checkCard(data_1, data_2)
 end
 
 -- return: allNums, {[icard]=nums}
-local function getCardAttr(card)
+local function getCardAttr_1(card)
 	local result = {}
 	local nums = 0
 	for _, data in pairs(card) do
@@ -87,69 +87,164 @@ local function getCardAttr(card)
 	return nums, result
 end
 
--- 单个
-handout[HANDOUT_DANGE] = function(seat, card)
-	local nums = getCardAttr(card)
-	if nums ~= 1 then 
+-- return: {icard}
+local function getCardAttr_2(card)
+	local result = {}
+	for _, data in pairs(card) do
+		for _, iCard in pairs(data) do
+			result[#result+1] = iCard
+		end
+	end
+	table.sort(result, function(A, B)
+		return A < B
+	end)
+	return result
+end
+
+-- 单个，对子，三个
+local function check_123(card, seat, isHandOut, iType)
+	local nums, resultCard_1 = getCardAttr_1(card)
+	local _, resultCard_2 = getCardAttr_1(lastHandOut)
+	if nums ~= iType then 
+		return 100
+	elseif not table.keyof(resultCard_1, iType) then 
 		return 100
 	end
-	return checkCard(allPlayerCard[seat], card)
+	local errorcode = checkCard(allPlayerCard[seat], card)
+	if isHandOut or errorcode ~= 0 then 
+		return errorcode 
+	end 
+	-- 检查大小 
+	for p, q in pairs(resultCard_1) do
+		for x, y in pairs(resultCard_2) do
+			if p <= x or q ~= y then 
+				return 100
+			end
+		end
+	end
+	return 0
+end
+
+-- 单个
+handout[HANDOUT_DANGE] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_DANGE then return 100 end
+	return check_123(card, seat, isHandOut, 1)
 end
 
 -- 对子
-handout[HANDOUT_DUIZI] = function(seat, card)
-	-- 检测是不是对子
-	local nums, resultCard = changeCard(card)
-	if nums ~= 2 then 
-		return 100
-	elseif not table.keyof(resultCard, 2) then 
-		return 100
-	end	
-	return errorcode = checkCard(allPlayerCard[seat], card)
+handout[HANDOUT_DUIZI] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_DUIZI then return 100 end
+	return check_123(card, seat, isHandOut, 2)
+end
+
+-- 三个
+handout[HANDOUT_SANGE] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_SANGE then return 100 end
+	return check_123(card, seat, isHandOut, 3)
 end
 
 -- 顺子
-handout[HANDOUT_SHUNZI] = function(seat, card)
+handout[HANDOUT_SHUNZI] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_SHUNZI then return 100 end
+	local resultCard = getCardAttr_2(card)
+	if #resultCard < 5 then return 100 end
 
+	-- 检查是不是顺子
+	for i=1, #resultCard do
+		local p = resultCard[i]
+		local q = resultCard[i+1]
+		if not q then 
+			break
+		end
+		if (q ~= p + 1) or (p >= 15 or q >= 15) then -- "2"以上不能做顺子
+			return 100
+		end
+	end
+	
+	local errorcode = checkCard(allPlayerCard[seat], card)
+	if isHandOut or errorcode ~= 0 then 
+		return errorcode 
+	end 
+
+	local lastResultCard = getCardAttr_2(lastHandOut)
+	if #resultCard ~= #lastResultCard then return 100 end
+
+	-- 判断是否大于上一手
+	if resultCard[1] <= lastResultCard[1] then return 100 end 
+	return errorcode
 end
 
 -- 连对
-handout[HANDOUT_LIANDUI] = function(seat, card)
+handout[HANDOUT_LIANDUI] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_LIANDUI then return 100 end
+	local nums_1, resultCard_1 = getCardAttr_1(card)
+	if nums_1 < 6 then return 100 end
 
+	for _, num in pairs(resultCard_1) do
+		if num ~= 2 then return 100 end
+	end
+	local resultCard_3 = getCardAttr_2(card)
+	local resultCard_4 = getCardAttr_2(lastHandOut)
+	-- 检查是不是顺
+	for i=1, #resultCard_3 do
+		local p = resultCard_3[i]
+		local q = resultCard_3[i+2]
+		if not q then 
+			break
+		end
+		if (q ~= p + 1) or (p >= 15 or q >= 15) then -- "2"以上不能做顺
+			return 100
+		end
+	end	
+	local errorcode = checkCard(allPlayerCard[seat], card)
+	if isHandOut or errorcode ~= 0 then 
+		return errorcode 
+	end 
+
+	local nums_2, _ = getCardAttr_1(lastHandOut)
+	if nums_1 ~= nums_2 then return 100 end
+
+	if resultCard_3[1] <= resultCard_4[1] then return 100 end 
+	return errorcode
 end
 
 -- 三带单
-handout[HANDOUT_SANDAI_1] = function(seat, card)
+handout[HANDOUT_SANDAI_1] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_SANDAI_1 then return 100 end
+	local nums_1, resultCard_1 = getCardAttr_1(card)
+	if nums_1 ~= 4 then return 100 end
+
 
 end
 
 -- 三带一对
-handout[HANDOUT_SANDAI_2] = function(seat, card)
+handout[HANDOUT_SANDAI_2] = function(seat, card, isHandOut)
+	if not isHandOut and lastHandOutType ~= HANDOUT_SANDAI_2 then return 100 end
 
 end
 
 -- 飞机带单
-handout[HANDOUT_FEIJI_1] = function(seat, card)
+handout[HANDOUT_FEIJI_1] = function(seat, card, isHandOut)
 
 end
 
 -- 飞机带对子
-handout[HANDOUT_FEIJI_2] = function(seat, card)
+handout[HANDOUT_FEIJI_2] = function(seat, card, isHandOut)
 
 end
 
 -- 四带二
-handout[HANDOUT_SIDAIER] = function(seat, card)
+handout[HANDOUT_SIDAIER] = function(seat, card, isHandOut)
 
 end
 
 -- 炸弹
-handout[HANDOUT_ZHADAN] = function(seat, card)
+handout[HANDOUT_ZHADAN] = function(seat, card, isHandOut)
 
 end
 
 -- 火箭
-handout[HANDOUT_HUOJIAN] = function(seat, card)
+handout[HANDOUT_HUOJIAN] = function(seat, card, isHandOut)
 
 end
 
@@ -195,7 +290,7 @@ function mod.handCardAuto(seat)
 	for idx, data in ipairs(card) do
 		if not iMinCard or iMinCard > data[1] then 
 			iMinCard = data[1]
-			iType = idx
+			iType = data.type
 		end
 	end
 	assert(iMinCard and iType, "handCardAuto error")
@@ -203,98 +298,113 @@ function mod.handCardAuto(seat)
 	local isWin = checkisWin(allPlayerCard[seat])
 	local result = {[1]={["flowcard"]={iMinCard},["type"]=iType}}
 	local nums = getCardNums(allPlayerCard[seat])
-	lastHandOut = {[iType] = {[1]=iMinCard}}
+	lastHandOut = {[iType] = {iMinCard}}
 	lastHandOutType = HANDOUT_DANGE
 	return result, HANDOUT_DANGE, nums, isWin
 end
 
 -- 跟牌
 -- card：协议格式
-function mod.followCard(seat, netCard, handType, isNotFollow)
+function mod.followCard(seat, netCard, handType, isHandOut)
 	local card = changeCardStyle(netCard)
 	print(">>>>>>>>>出牌，类型：", handType)
-	local errorcode = handout[handType](seat, card)
-	if isNotFollow then 
-		return errorcode
-	end
+	local errorcode = handout[handType](seat, card, isHandOut)
+	lastHandOut = errorcode == 0 and card or lastHandOut
+	lastHandOutType = errorcode == 0 and handType or lastHandOutType
+	return errorcode
 end
 
 ----------------------------------------
 -- for test
 ----------------------------------------
--- local pCard = {
--- 	[1] = {
--- 		[1] = {
--- 			["card"] = {
--- 				[1] = 13,
--- 				[2] = 4,
--- 				[3] = 5,
--- 			},
--- 			["type"] = 1,
--- 		},
--- 		[2] = {
--- 			["card"] = {
--- 				[1] = 15,
--- 				[2] = 8,
--- 				[3] = 3,
--- 			},
--- 			["type"] = 2,
--- 		},
--- 		[3] = {
--- 			["card"] = {
--- 				[1] = 15,
--- 				[2] = 14,
--- 				[3] = 7,
--- 			},
--- 			["type"] = 3,
--- 		},
--- 	},
--- }
+local pCard = {
+	[1] = {
+		[1] = {
+			["card"] = {
+				[1] = 4,
+				[2] = 5,
+				[3] = 6,
+				[4] = 7,
+			},
+			["type"] = 1,
+		},	
+		[2] = {
+			["card"] = {
+				[1] = 4,
+				[2] = 5,
+				[3] = 6,
+				[4] = 7,
+			},
+			["type"] = 2,
+		},
+		[3] = {
+			["card"] = {
+				[1] = 15,
+				[2] = 14,
+				[3] = 7,
+			},
+			["type"] = 3,
+		},
+	},
+}
 
--- local dizhu = {
--- 	[1] = {
--- 		["card"] = {
--- 			[1] = 8,
--- 			[2] = 9,
--- 		},
--- 		["type"] = 1,
--- 	},
--- 	[2] = {
--- 		["card"] = {
--- 			[1] = 16,
--- 		},
--- 		["type"] = 5,
--- 	},
--- }
+local dizhu = {
+	[1] = {
+		["card"] = {
+			[1] = 8,
+			[2] = 9,
+		},
+		["type"] = 1,
+	},
+	[2] = {
+		["card"] = {
+			[1] = 16,
+		},
+		["type"] = 5,
+	},
+}
 
--- mod.init(pCard, dizhu, 1)
+mod.init(pCard, dizhu, 1)
 -- print(dump(allPlayerCard))
 -- mod.handCardAuto(1)
 -- print(dump(allPlayerCard))
 
-local data_1 = {
+local netCard = {
 	[1] = {
-		[1] = 3,
-		[2] = 4,
-		[3] = 5,
-	},
+		["card"] = {
+			[1] = 4,
+			[2] = 5,
+			[3] = 6,
+			[4] = 7,
+		},
+		["type"] = 1,
+	},	
 	[2] = {
-		[1] = 6,
-		[2] = 8,
-		[3] = 9,
+		["card"] = {
+			[1] = 4,
+			[2] = 5,
+			[3] = 6,
+			[4] = 7,
+		},
+		["type"] = 2,
 	},
+	-- [3] = {
+	-- 	["card"] = {
+	-- 		[1] = 7,
+	-- 	},
+	-- 	["type"] = 3,
+	-- },
 }
-local data_2 = {
-	[1] = {
-		[1] = 3,
-	},
-	[2] = {
-		[1] = 6,
-	},
-	[4] = {
-		[1] = 10,
-	},
-}
-print(checkCard(data_1, data_2))
+
+-- local handType = HANDOUT_LIANDUI
+-- local isHandOut = false
+-- lastHandOutType = HANDOUT_LIANDUI 
+-- lastHandOut = {
+-- 	[1] = {3,4,5,6},
+-- 	[2] = {3,4,5,6},
+-- }
+
+-- local errorcode = mod.followCard(1, netCard, handType, isHandOut)
+-- print(">>>>>>>>>>>>>>>>>>", errorcode)
 
 return mod
